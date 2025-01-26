3cd9844a97289d80b610488e3d92f4519a48536b
Amit Kumar Sahoo <amit200130@gmail.com>
2025-01-26 16:51:32 +0000

    datatypes in js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "oggyjack32@gmail.com"
+var accountPassowerd = "12345"
+accountCity ="Jagatsinghpur"
+let accountState
+
+//accountd = 2
+accountEmail = "Dor@emon.com"
+accountPassword = "2121212121"
+accountCity="Bengaluru"
+ console.log(accountId);
+ console.table([accountEmail,accountId,accountPassowerd,accountCity,accountState])
---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use srtrict";//treat all js as newer version
+//alert 3 +3//we are usung nodejs, note browser
+console.log (3+3)
+console.log("Amit")
+let name= "Amit"
+let age =  23
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint
+// string=""
+// boolean =>true/false
+//null => standalone value
+//undefined=>
+    //symbol=>unique
+//object
+console.log(typeof "Amit");
+console.log(typeof 23)
+console.log(typeof null)
+console.log(typeof undefined);
+
